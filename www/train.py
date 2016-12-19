dadda4133b3b13e41a8ff1ad03e6f6e9f5dcc47e
DevGuan <975081801@qq.com>
2016-12-19 19:25:36 +0800

    边学边修改

--- www/train.py
+++ www/train.py
@@ -1,17 +1,58 @@
 # coding=utf-8
 
 from bottle import *
-
+import json
 '''
-	使用POST方法 验证登录指令
+    使用POST方法 验证登录指令
 '''
-@route('/login/<name>',method='GET')
-def login(name):
+@post('/login',method='POST')
+def login():
+    params = request.body.read()
+    print 'data---' + str(params)
+    try:
+    	params = json.loads(params)
+    except Exception as e:
+    	params = {}
 
-    # postValue = bottle.request.POST.decode('utf-8')
-    # userName = bottle.request.POST.get('username')
-    # password = bottle.request.POST.get('password')
+    # postValue = request.POST.decode(encoding ='utf-8')
+    userName = params.get('username')
+    password = params.get('password')
         
-    return 'zhengchang  --- %s'% name
+    canLogin = checkLogin('123456','123456')
+    data = {"code":"0","username":userName,'password':password}
+    jsondata = json.dumps(data)
+
+    print jsondata
+    
+    if canLogin:
+    	# return {'code':'success'} 
+        return jsondata
+    else:
+        return {'code':'1'}
+
+    # return {'code':'success'}    
+    # return username + '密码' + password
+    
+
+def checkLogin(username,password):
+    username = username
+    password = password
+
+    if username == '123456' and password == '123456':
+        return True
+    else:
+        return False
+    
+
+@route('/regist',method='POST')
+def regist():
+    postValue = bottle.request.POST.decode('utf-8')
+    userName = bottle.request.POST.get('username')
+    password = bottle.request.POST.get('password')
+    checkCode = bottle.request.POST.get('checkCode')
+    '''
+    检查数据库中是否已有账号  判断是否可以注册成功
+    '''
+    return {'code':'success'}
 
 run(host='0.0.0.0',port=8888)
